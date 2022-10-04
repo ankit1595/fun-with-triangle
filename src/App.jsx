c628297721eda261435ef969661ef458349171cc
Ankit <ankit.95sharma@gmail.com>
2022-10-04 16:54:04 +0530

    feat: footer added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,32 @@ function App() {
       {/* <TriangleQuiz /> */}
       {/* <Hypotenuse /> */}
       <TriangleArea />
+      <footer>
+        <p>© | 2021 | Ankit Sharma</p>
+        <ul className="list-unordered inline-list">
+          <li>
+            <a
+              className="links"
+              href="https://github.com/ankit1595/"
+              target="_blank">
+              <img src="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="links"
+              href="https://twitter.com/ankit1595"
+              target="_blank">
+              <img src="" alt="twitter" />
+            </a>
+          </li>
+          <li>
+            <a className="links" href="" target="_blank">
+              <img src="" alt="" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
