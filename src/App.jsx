bfe78f8484f7200e393c87baeb51baef56d5957b
Ankit <ankit.95sharma@gmail.com>
2022-10-04 17:13:48 +0530

    feat: nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,47 @@ import Hypotenuse from "./components/Hypotenuse";
 import IsTriangle from "./components/IsTriangle";
 import TriangleQuiz from "./components/TriangleQuiz";
 import TriangleArea from "./components/TriangleArea";
+import { useState } from "react";
 
 function App() {
+  const defaultNav = "nav1";
+  const [nav, setNav] = useState(defaultNav);
   return (
     <div className="App">
-      {/* <IsTriangle /> */}
-      {/* <TriangleQuiz /> */}
-      {/* <Hypotenuse /> */}
-      <TriangleArea />
+      <h1>Fun with Triangle</h1>
+      <nav>
+        <ul>
+          <li
+            style={{ cursor: "pointer" }}
+            onClick={() => setNav("nav1")}
+            className={nav === "nav1" && "active"}>
+            Is Triangle?
+          </li>
+
+          <li
+            style={{ cursor: "pointer" }}
+            onClick={() => setNav("nav2")}
+            className={nav === "nav2" && "active"}>
+            Triangular Quiz
+          </li>
+          <li
+            style={{ cursor: "pointer" }}
+            onClick={() => setNav("nav3")}
+            className={nav === "nav3" && "active"}>
+            Calculate Hypotenuse
+          </li>
+          <li
+            style={{ cursor: "pointer" }}
+            onClick={() => setNav("nav4")}
+            className={nav === "nav4" && "active"}>
+            Calculate Area
+          </li>
+        </ul>
+      </nav>
+      {nav === "nav1" && <IsTriangle />}
+      {nav === "nav2" && <TriangleQuiz />}
+      {nav === "nav3" && <Hypotenuse />}
+      {nav === "nav4" && <TriangleArea />}
       <footer>
         <p>© | 2021 | Ankit Sharma</p>
         <ul className="list-unordered inline-list">
